414e5dc6180332bf43cc77bc1c8b9277eec1aeb3
leoo1995 <43155897+leoo1995@users.noreply.github.com>
2022-10-10 00:32:36 -0500

    linter errors fixed

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,22 +1,22 @@
 export interface Post {
-  id: number
+  id: number | string
   name: string
   image: string
   body: string
   photoUser: string
   likes: number
   timesShared: number
-  createdAt: Date
+  createdAt: Date | string
   comments: Comment[]
-  alsoLikedTo: {
-    id: string
+  alsoLikedTo?: {
+    id: string | number
     src: string
     name: string
   }[]
 }
 
 export interface Comment {
-  id: number
+  id: string | number
   title: string
   src: string
   message: string
